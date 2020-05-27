2e538020daf73f70392a84b498e79f758f79bd55
zbattaglia <zbattaglia3@gmail.com>
2020-05-27 10:43:27 -0500

    Can send predetermined accept/reject messages to user's requesting you to cover their shifts

--- src/redux/sagas/message.js
+++ src/redux/sagas/message.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { put, takeLatest } from 'redux-saga/effects';
 
-// worker Saga: will be fired on "FETCH_USER" actions
+// worker Saga: will be fired on "FETCH_MESSAGE" actions
 function* fetchMessage() {
     console.log( 'In fetchMessage Saga' );
   try {
@@ -14,8 +14,33 @@ function* fetchMessage() {
   }
 }
 
+// saga will be fired on "DELETE_MESSAGE" actions
+function* deleteMessage(action) {
+  // console.log( `In delete message saga with messge id: ${action.payload}`);
+  try {
+    yield axios.delete( `/message/${action.payload}` );
+
+    yield put( { type: 'FETCH_MESSAGE' } );
+  }
+  catch(error) {
+    console.log( 'Error deleting message', error );
+  }
+}
+
+function* replyToMessage(action) {
+  console.log( `Replying to message with id ${action.payload.id} in reply saga with ${action.payload.type}`);
+  try {
+    yield axios.post( '/message', action.payload );
+  }
+  catch(error) {
+    console.log( `Error replying to message`, error );
+  }
+}
+
 function* messageSaga() {
   yield takeLatest('FETCH_MESSAGE', fetchMessage);
+  yield takeLatest('DELETE_MESSAGE', deleteMessage);
+  yield takeLatest('REPLY_TO_MESSAGE', replyToMessage);
 }
 
 export default messageSaga;
