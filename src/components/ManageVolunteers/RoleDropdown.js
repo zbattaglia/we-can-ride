6275e46e1f27aa7b5a08b56b2bd0d0c63a1aeb95
Erick Jensen <Erick.Jensen010@gmail.com>
2020-05-28 15:49:18 -0500

    add dropdown component

---
+++ src/components/ManageVolunteers/RoleDropdown.js
@@ -0,0 +1,121 @@
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import ListItemText from '@material-ui/core/ListItemText';
+import Select from '@material-ui/core/Select';
+import Checkbox from '@material-ui/core/Checkbox';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Chip from '@material-ui/core/Chip';
+
+const styles = theme => ({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    formControl: {
+      margin: theme.spacing.unit,
+      minWidth: 120,
+      maxWidth: 300,
+    },
+    chips: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    chip: {
+      margin: theme.spacing.unit / 4,
+    },
+    noLabel: {
+      marginTop: theme.spacing.unit * 3,
+    },
+  });
+  
+  const ITEM_HEIGHT = 48;
+  const ITEM_PADDING_TOP = 8;
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 250,
+      },
+    },
+  };
+  
+  const names = [
+    'sidewalker',
+    'leader',
+    'barn aid',
+    'feeder',
+  ];
+  
+  function getStyles(name, that) {
+    return {
+      fontWeight:
+        that.state.name.indexOf(name) === -1
+          ? that.props.theme.typography.fontWeightRegular
+          : that.props.theme.typography.fontWeightMedium,
+    };
+  }
+  
+
+
+
+  class RoleDropdown extends React.Component {
+    state = {
+        name: [],
+      };
+    
+      handleChange = event => {
+        this.setState({ name: event.target.value });
+      };
+    
+      handleChangeMultiple = event => {
+        const { options } = event.target;
+        const value = [];
+        for (let i = 0, l = options.length; i < l; i += 1) {
+          if (options[i].selected) {
+            value.push(options[i].value);
+          }
+        }
+        this.setState({
+          name: value,
+        });
+      };
+    
+      render() {
+        const { classes } = this.props;
+    
+        return (
+          <div className={classes.root}>
+            <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="select-multiple-checkbox">Role</InputLabel>
+          <Select
+            multiple
+            value={this.state.name}
+            onChange={this.handleChange}
+            input={<Input id="select-multiple-checkbox" />}
+            renderValue={selected => selected.join(', ')}
+            MenuProps={MenuProps}
+          >
+            {names.map(name => (
+              <MenuItem key={name} value={name}>
+                <Checkbox checked={this.state.name.indexOf(name) > -1} />
+                <ListItemText primary={name} />
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => ({
+    state,
+    classes: PropTypes.object.isRequired,
+});
+
+export default connect(mapStateToProps)(withStyles(styles, { withTheme: true })(RoleDropdown));
