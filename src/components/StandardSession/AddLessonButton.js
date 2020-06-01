23943e949e3115b0341f49028a4ed9c6a5747bab
Frieda Jacobson <kajakali@hotmail.com>
2020-05-31 22:16:24 -0500

    all the  parts are in the modal they just require connection

--- src/components/StandardSession/AddLessonButton.js
+++ src/components/StandardSession/AddLessonButton.js
@@ -21,6 +21,7 @@ import PropTypes from 'prop-types';
 import Checkbox from '@material-ui/core/Checkbox';
 
 
+
 const styles = theme => ({
   root: {
     display: 'flex',
@@ -59,9 +60,10 @@ const styles = theme => ({
 class AddLessonButton extends Component {
   state = {
     open: false,
-    time: '',
+    start_time: '',
     day: '',
-    length: '',
+    duration: '',
+    client: '',
   };
 
   handleClickOpen = () => {
@@ -70,7 +72,8 @@ class AddLessonButton extends Component {
 
   handleClose = (blob) => {
     if(blob === 'create'){
-      this.props.dispatch({ type: 'CREATE_SESSION', payload: {date: this.state.date, yearlong: this.state.yearlong, length: this.state.length}});
+      console.log('send the state to the server', this.state);
+      //this.props.dispatch({ type: 'CREATE_SESSION', payload: {date: this.state.date, yearlong: this.state.yearlong, length: this.state.length}});
     }
     this.setState({ open: false });
   };
@@ -90,6 +93,15 @@ class AddLessonButton extends Component {
 
   render() {
     const { classes } = this.props;
+    const weekdays = [
+      {day: 'Monday', sqlDate: '1996-01-01'},
+      {day: 'Tuesday', sqlDate: '1996-01-02'},
+      {day: 'Wednesday', sqlDate: '1996-01-03'},
+      {day: 'Thursday', sqlDate: '1996-01-04'},
+      {day: 'Friday', sqlDate: '1996-01-05'},
+      {day: 'Saturday', sqlDate: '1996-01-06'},
+      {day: 'Sunday', sqlDate: '1996-01-07'}
+    ]
    
 return (
   <>
@@ -104,6 +116,53 @@ return (
     <DialogContentText>
       Create a new lesson
     </DialogContentText>
+    {/**we need a day of the week select that maps over an array of days with associated 
+     * days in 1996, as well as a time picker and a picker for the interval - another time picker?!
+     * 
+     * a seelct that maps throguh weekdays
+
+
+     */}
+      <Select
+            value={this.state.day}
+            onChange={this.handleChange}
+            inputProps={{
+              name: 'day',
+              id: 'day',
+            }}
+          >
+            {weekdays.map( weekday => (
+              <MenuItem value={weekday.sqlDate}>{weekday.day}</MenuItem>
+            ))}
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+     <TextField
+      label='Client'
+      type='text'
+     />
+
+<TextField
+      label='Lesson Length'
+      type='number'
+     />
+           <TextField
+        id="time"
+        label="Lesson Start Time"
+        type="time"
+        defaultValue="07:30"
+        className={classes.textField}
+        InputLabelProps={{
+          shrink: true,
+        }}
+        inputProps={{
+          step: 300, // 5 min
+        }}
+      />
 <TextField
 id="date"
 label="Start Date"
@@ -139,7 +198,7 @@ value="yearlong"
       Cancel
     </Button>
     <Button onClick={() => this.handleClose('create')} color="primary">
-      Create New Session
+      Create Lesson
     </Button>
   </DialogActions>
 </Dialog>
