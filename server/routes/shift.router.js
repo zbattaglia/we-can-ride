ef17980c2e817d825d66da918682d2598ab055f7
w4bb4j4ck <blyblz5@gmail.com>
2020-06-08 16:39:47 -0500

    Change get shifts query

--- server/routes/shift.router.js
+++ server/routes/shift.router.js
@@ -62,8 +62,8 @@ router.get('/fourweeks', rejectUnauthenticated, (req, res) => {
 });
 
 router.get(`/all`, rejectUnauthenticated, (req, res) => {
-    const sqlText = `SELECT "shift"."date", "user"."first_name", "user"."last_name", "lesson"."start_of_lesson", 
-    "shift"."assigned_user", "shift"."user_wants_to_trade", "skill"."title" FROM "shift"
+    const sqlText = `SELECT "shift"."date", "user"."first_name", LEFT("user"."last_name", 1) AS "last_name", 
+    "lesson"."start_of_lesson", "shift"."assigned_user", "shift"."user_wants_to_trade", "skill"."title" FROM "shift"
     LEFT JOIN "user" ON "shift"."assigned_user" = "user"."id"
     JOIN "slot" ON "shift"."slot_id" = "slot"."id"
     JOIN "lesson" ON "slot"."lesson_id" = "lesson"."id"
