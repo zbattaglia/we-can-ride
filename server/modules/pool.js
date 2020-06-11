fdac3751b59754484fbe7f0fbe297cb87aa38ed7
zbattaglia <zbattaglia3@gmail.com>
2020-06-11 15:14:38 -0500

    initial commit

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -25,7 +25,7 @@ if (process.env.DATABASE_URL) {
   config = {
     host: 'localhost', // Server hosting the postgres database
     port: 5432, // env var: PGPORT
-    database: 'we_can_ride', // this is the name of the database you're using
+    database: process.env.DATABASE_NAME || 'we_can_ride', // this is the name of the database you're using
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
   };
